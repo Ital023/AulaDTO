968388ec0de0d2c68b1853e31f34d0e9d63b8f75
Ital023 <contadoitalo911@gmail.com>
2024-10-02 11:39:55 -0300

    feat: User service

---
+++ backend/src/main/java/io/github/ital023/AulaDTOItal023/services/UserService.java
@@ -0,0 +1,21 @@
+package io.github.ital023.AulaDTOItal023.services;
+
+import io.github.ital023.AulaDTOItal023.entities.User;
+import io.github.ital023.AulaDTOItal023.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+
+}
