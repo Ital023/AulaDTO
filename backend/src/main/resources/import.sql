b36846c035beee1756c539f1bba854dc424b9b29
Ital023 <contadoitalo911@gmail.com>
2024-10-02 11:47:52 -0300

    feat: Seeding data

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_user(id, username, description, password, email, img_url) VALUES (1, 'Italo Miranda', 'Desenvolvedor Fullstack', '123456', 'italo@gmail.com','https://avatars.githubusercontent.com/u/113559117?v=4');
+INSERT INTO tb_user(id, username, description, password, email, img_url) VALUES (2, 'Gabriel Meneses', 'Desenvolvedor Backend', '123456', 'gabriel@gmail.com','https://avatars.githubusercontent.com/u/98995360?v=4');
+INSERT INTO tb_user(id, username, description, password, email, img_url) VALUES (3, 'Davi Cotting', 'Desenvolvedor Front End', '123456', 'davi@gmail.com','https://avatars.githubusercontent.com/u/122418493?v=4');
+INSERT INTO tb_user(id, username, description, password, email, img_url) VALUES (4, 'Carlos Alexandre', 'Desenvolvedor Fullstack', '123456', 'carlos@gmail.com','https://avatars.githubusercontent.com/u/143346006?v=4');
