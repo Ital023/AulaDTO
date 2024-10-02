16042b88a8ad9392ec0c4d2dcd3748d520f45b6e
Ital023 <contadoitalo911@gmail.com>
2024-10-02 11:24:31 -0300

    initial files backend

---
+++ backend/src/main/java/io/github/ital023/AulaDTOItal023/AulaDtoItal023Application.java
@@ -0,0 +1,13 @@
+package io.github.ital023.AulaDTOItal023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AulaDtoItal023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AulaDtoItal023Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/ital023/AulaDTOItal023/AulaDtoItal023ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ital023.AulaDTOItal023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulaDtoItal023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
