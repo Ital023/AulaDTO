700660e12b29d4ad9086a77c2bb7e2bb95b37976
Ital023 <contadoitalo911@gmail.com>
2024-10-08 21:26:47 -0300

    feat: att iframe

--- frontend/src/routes/Home/HomeBody/index.tsx
+++ frontend/src/routes/Home/HomeBody/index.tsx
@@ -5,8 +5,8 @@ export default function HomeBody() {
       <h2 className="text-2xl">Aprenderemos como consumir Endpoints de Api com DTO</h2>
 
       <p>Assista a aula:</p>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/ah06sWCILVU?si=9CkwIZOVtgcjyOG0" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
-      <a className="underline text-blue-500" href="https://youtu.be/ah06sWCILVU" target="_blank">Acessar a aula</a>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/Yw4TwGho320?si=UA0Pk6bAccHUdozz" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      <a className="underline text-blue-500" href="https://www.youtube.com/watch?v=Yw4TwGho320" target="_blank">Acessar a aula</a>
     </main>
   );
 }
